7c7637daf57b0b940c74de337965f00d7faffbb1
dkat-davi <eudkatdavikalel@gmail.com>
2023-01-07 17:47:17 -0300

    Add gameOverScreen

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const screenGameOver = document.querySelector('.game-over-screen');
+const newGameButton = document.querySelector('.new-game');
+const body = document.querySelector('body')
+
+function refresh() {
+    document.location.reload(true);
+}
 
 const jump = () => {
     mario.classList.add('jump')
@@ -14,7 +21,7 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 139  && pipePosition > 0 && marioPosition < 104) {
+    if (pipePosition <= 138  && pipePosition > 0 && marioPosition < 104) {
             pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`
 
@@ -22,11 +29,18 @@ const loop = setInterval(() => {
             mario.src = "images/game-over.png"
             mario.style.width = '75px'
             mario.style.marginLeft = '60px'
-            mario.classList.add('gameOver')
-            mario.style.bottom = '-150px';
+            mario.classList.add('marioGameOver') 
+            mario.style.bottom = '-15rem';
             
+            /*Game Over screen*/
+            screenGameOver.style.display = 'flex'
+            screenGameOver.style.animation = 'screenGameOverAnimation 1.5s linear'
+
             clearInterval(loop)
     }
 }, 10)
 
+newGameButton.addEventListener('click', refresh);
+body.addEventListener('click', jump)
 document.addEventListener('keydown', jump);
+
