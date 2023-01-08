8704aa466d2f1a5a47193c18969fe69ac8425034
dkat-davi <eudkatdavikalel@gmail.com>
2023-01-08 09:58:01 -0300

    Creating score

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ const pipe = document.querySelector('.pipe');
 const screenGameOver = document.querySelector('.game-over-screen');
 const newGameButton = document.querySelector('.new-game');
 const body = document.querySelector('body')
+const score = document.querySelector('.score')
+const finalScore = document.querySelector('.points')
 
+/* Refresh page */
 function refresh() {
     document.location.reload(true);
+}
+
+let count = 0;
+function points() {
+    count += 1;
+    score.innerHTML = `Score = ${count}`
+    finalScore.innerHTML = `Score = ${count}`
+    console.log('Funfou')
 }
 
 const jump = () => {
@@ -16,15 +27,21 @@ const jump = () => {
     }, 700)
 }
 
+/* Loop Game */
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
+
+    /* Game Over */
     if (pipePosition <= 138  && pipePosition > 0 && marioPosition < 104) {
+            
+            /* Pipe game over animation */
             pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`
 
+            /* Mario game over animation */
             mario.style.bottom = `${marioPosition}px`;
             mario.src = "images/game-over.png"
             mario.style.width = '75px'
@@ -36,7 +53,11 @@ const loop = setInterval(() => {
             screenGameOver.style.display = 'flex'
             screenGameOver.style.animation = 'screenGameOverAnimation 1.5s linear'
 
+            /* Stop Loop */
             clearInterval(loop)
+    }
+    else {
+        points();
     }
 }, 10)
 
